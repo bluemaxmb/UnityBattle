885837c7e8b7c9e5c742e50dfebeadf31ef07e18
Max Blue <bluemaxmb@gmail.com>
2018-01-21 02:22:43 -0800

    # Minor clean up
    Removed a bunch of unneeded code.

--- Assets/Scripts/BattleSystem/EnemyTargetButton.cs
+++ Assets/Scripts/BattleSystem/EnemyTargetButton.cs
@@ -1,11 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine.UI;
 
 public class EnemyTargetButton : Button 
 {
 	public EnemyBattleParticipant battleParticipant { get; set; }
-
-
 }
